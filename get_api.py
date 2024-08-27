e4f68f1738abdf4a60d10e1cb88400335eacb535
satoshi1979 <satoshi19791212@gmail.com>
2024-08-27 22:38:55 +0900

    市町村コード取得成功

--- get_api.py
+++ get_api.py
@@ -4,7 +4,7 @@ api_key = "0iKaDKQrdMpKRS2LVhDifNC8QxMWDASPp9HVlnB7"
 headers = {"X-API-KEY": api_key}
 
 
-response = requests.get("https://opendata.resas-portal.go.jp/api/v1/municipality/value/perYear?simcCode=51&cityCode=13101&year=2012&prefCode=13&sicCode=I", headers=headers)
+response = requests.get("https://opendata.resas-portal.go.jp/api/v1/prefectures", headers=headers)
 data = response.json()
 
 
